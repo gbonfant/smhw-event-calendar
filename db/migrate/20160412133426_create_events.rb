713ffa28ad4337f9304e9e230dbfc830de9e1988
Gus Bonfant <gbonfant@me.com>
2016-04-12 14:35:27 +0100

    Add migration for Event model

---
+++ db/migrate/20160412133426_create_events.rb
@@ -0,0 +1,9 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.datetime :start_date
+      t.datetime :end_date
+    end
+  end
+end
