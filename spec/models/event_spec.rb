7c1e108ff17f91662ad952d2c291a13289d9e73e
Gus Bonfant <gbonfant@me.com>
2016-04-12 15:13:27 +0100

    Refactor Event specs.
    Set a default value in the db for event titles

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 describe Event do
-  it 'validates the presence of title' do
-    params = { start_date: DateTime.now, end_date: 2.days.from_now }
-
-    expect(Event.create(params)).to be_invalid
-  end
-
   it 'validates the presence of a starting date' do
     params = { title: 'Event title', end_date: 2.days.from_now }
 
