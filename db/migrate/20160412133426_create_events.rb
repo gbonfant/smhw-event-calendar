323cb3e414d843e599c89487a8b6ef922153061d
Gus Bonfant <gbonfant@me.com>
2016-04-12 15:11:31 +0100

    Implement JS solution for calendar.
    Using [fullcalendar](http://fullcalendar.io/) for rendering the calendar and
    events.

--- db/migrate/20160412133426_create_events.rb
+++ db/migrate/20160412133426_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :title
+      t.string :title, default: 'New Event'
       t.datetime :start_date
       t.datetime :end_date
     end
